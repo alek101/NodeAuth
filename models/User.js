55d0f9ade16a88ff0061b723d2a05060fbde010f
alek101 <47416711+alek101@users.noreply.github.com>
2021-06-17 08:04:44 +0200

    #6
    mongoose hooks

--- models/User.js
+++ models/User.js
@@ -16,6 +16,21 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+//fire a function after doc saved to db
+
+userSchema.post('save', function(doc, next) {
+    console.log('new user was created and saved', doc)
+    next()
+})
+
+//fire a function before doc saved to db
+//this is releted to the user instance
+userSchema.pre('save', function(next) {
+    console.log('user about to be created & saved', this)
+
+    next();
+})
+
 const User = mongoose.model('user', userSchema);
 
 module.exports = User;
